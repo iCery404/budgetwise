01fe8de065684d7856d23e7ef2579246d0893746
iCery404 <jhustinemalawit308@gmail.com>
2026-09-19 01:09:24 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,7 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/budgetwise/',
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-      },
-    },
-  },
 })
